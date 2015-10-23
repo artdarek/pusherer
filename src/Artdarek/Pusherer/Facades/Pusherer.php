c5dd902d89a5d9074dd5403a8adec76e8e809919
Gabriel Pedro <gpedro831@gmail.com>
2015-10-23 16:16:49 -0300

    Fixing mixed identation and trailing spaces, Updated for Pusher-PHP 2.2.x

--- src/Artdarek/Pusherer/Facades/Pusherer.php
+++ src/Artdarek/Pusherer/Facades/Pusherer.php
@@ -1,14 +1,15 @@
-<?php namespace Artdarek\Pusherer\Facades;
+<?php
+namespace Artdarek\Pusherer\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 class Pusherer extends Facade {
 
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor() { return 'pusherer'; }
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'pusherer'; }
 
 }
